cb7309c7727637edfb59f283d5faa1d9d454afa4
songxiaoyuan <xiaoyuan@nfs.iscas.ac.cn>
2016-04-20 15:23:29 +0800

    add the menubar.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var main = {
+  initialize:function() {
+  	menubar.initialize();
+  	projectStructure.initialize();
+  	console.log(test);
+  	console.log(menubar.test);
+  }	
+};
+
+main.initialize();
---
+++ js/menubar.js
@@ -0,0 +1,7 @@
+var menubar ={
+	test:12;
+	initialize:function() {
+  	  var menubar = "this is menubar";
+  	  $('#menubar').html(menubar);
+    }	
+}
