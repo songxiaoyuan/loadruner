b0668a20f64bfb6ceb5411a2c94ba6f937da1ada
songxiaoyuan <xiaoyuan@nfs.iscas.ac.cn>
2016-06-23 09:40:50 +0800

    start the node-java

--- js/node-java/project.js
+++ js/node-java/project.js
@@ -1,12 +1,12 @@
 var java = require('java');
 
-//lxw: 1. classpath.push is essential here, to find the specific/customized class(src.Hello).
+//sxy: 1. classpath.push is essential here, to find the specific/customized class(src.Hello).
 //     2. class.push() statements MUST be put in front of all the "import()" and "newInstance()" statements.
-//lxw: NOTE here, the current working directory is the directory of file(aka. index.html) calling project.js instead of the directory of "project.js".
+//sxy: NOTE here, the current working directory is the directory of file(aka. index.html) calling project.js instead of the directory of "project.js".
 //java.classpath.push("../../bin")	//NO   cannot find the class Project.
 java.classpath.push("bin");
 //push jar files into classpath.
-//lxw NOTE: must restrict to the jar file, intead of the directory.
+//sxy NOTE: must restrict to the jar file, intead of the directory.
 java.classpath.push("javaSrc/thirdPartyJARs/gson-2.3.1.jar");
 java.classpath.push("javaSrc/thirdPartyJARs/xstream-1.4.9.jar");
 
@@ -15,12 +15,12 @@ var Project = java.import('Project');
 //var project = new Project();
 var project = Project.getInstanceSync();
 
-/*
+
 //1. Create Project.
 //TODO: the jsonString should be from the frontend.
 var jsonString = "{\"projectName\": \"DWL\", " + 
-				 "\"savedPath\": \"/home/lxw/DWLProj\", " + 
-				 "\"author\": \"lxw\", " + 
+				 "\"savedPath\": \"/home/sxy/DWLProj\", " + 
+				 "\"author\": \"sxy\", " + 
 				 "\"date\": \"2016.06.02\", " + 
 				 "\"comments\": \"No comments\"}";
 var result = project.createProjectSync(jsonString);
@@ -28,67 +28,67 @@ if(result == 0)
 	console.log(result + " Project is created successfully.");
 else
 	console.log(result + " [Error] Creating Errors")
-*/
+
 
 //2. Import Project.
-result = project.importProjectSync("/home/lxw/MovedDWLProj");
+result = project.importProjectSync("/home/sxy/TmpProj");
 if(result == 0)
-	console.log(result + " /home/lxw/MovedDWLProj is imported successfully.\n");
+	console.log(result + " /home/sxy/TmpProj is imported successfully.\n");
 else
 	console.log(result + " [Error] Importing Errors")
 
 //4. Save Project.
 result = project.saveProjectSync();
 if(result == 0)
-	console.log(result + " /home/lxw/MovedDWLProj is saved successfully.");
+	console.log(result + " /home/sxy/TmpDWLProj is saved successfully.");
 else
 	console.log(result + " [Error] Save Errors");
 
 //3. Open Project.
-result = project.openProjectSync("/home/lxw/DWLProj");
+result = project.openProjectSync("/home/sxy/DWLProj");
 if(result == 0)
-	console.log(result + " /home/lxw/DWLProj is opened successfully.\n");
+	console.log(result + " /home/sxy/DWLProj is opened successfully.\n");
 else
 	console.log(result + " [Error] Open Errors");
 
 //4. Save Project.
 result = project.saveProjectSync();
 if(result == 0)
-	console.log(result + " /home/lxw/DWLProj is saved successfully.");
+	console.log(result + " /home/sxy/DWLProj is saved successfully.");
 else
 	console.log(result + " [Error] Save Errors");
 
 //2. Import Project.
-result = project.importProjectSync("/home/lxw/MovedDWLProj");
+result = project.importProjectSync("/home/sxy/TmpProj");
 if(result == 0)
-	console.log(result + " /home/lxw/MovedDWLProj is imported successfully.\n");
+	console.log(result + " /home/sxy/TmpProj is imported successfully.\n");
 else
 	console.log(result + " [Error] Importing Errors")
 
 //4. Save Project.
 result = project.saveProjectSync();
 if(result == 0)
-	console.log(result + " /home/lxw/MovedDWLProj is saved successfully.");
+	console.log(result + " /home/sxy/TmpProj is saved successfully.");
 else
 	console.log(result + " [Error] Save Errors");
 
 //3. Open Project.
-result = project.openProjectSync("/home/lxw/DWLProj");
+result = project.openProjectSync("/home/sxy/DWLProj");
 if(result == 0)
-	console.log(result + " /home/lxw/DWLProj is opened successfully.\n");
+	console.log(result + " /home/sxy/DWLProj is opened successfully.\n");
 else
 	console.log(result + " [Error] Open Errors");
 
 //4. Save Project.
 result = project.saveProjectSync();
 if(result == 0)
-	console.log(result + " /home/lxw/DWLProj is saved successfully.");
+	console.log(result + " /home/sxy/DWLProj is saved successfully.");
 else
 	console.log(result + " [Error] Save Errors");
 
 //5. Close Project.
 project.closeProjectSync();
-console.log("/home/lxw/DWLProj is closed successfully.");
+console.log("/home/sxy/DWLProj is closed successfully.");
 
 //Show the opened Projects.
 console.log("The opened Projects are as follows: ");
